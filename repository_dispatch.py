a5d4789817ef7f646cfb4bc244566121a699c87d
petermeissner <retep.meissner@gmail.com>
2021-01-03 21:48:41 +0100

    manual workflow dispatch via Python Script

--- repository_dispatch.py
+++ repository_dispatch.py
@@ -1,10 +1,32 @@
 import requests
 
 
-url = "https://api.github.com/repos/petermeissner/github-actions-course/dispatches"
-requests.post(
+token = open('github_token.txt', 'r').read().replace("\n", "")
+
+url = "https://api.github.com/repos/petermeissner/github-actions-course/actions/workflows/05_repository_dispatch.yml/dispatches"
+r = requests.post(
   url=url, 
-  headers={"accept": "application/vnd.github.v3+json"},
-  data={"event_type": "manual_trigger"}
+  headers=
+    {
+      "Accept": "application/vnd.github.v3+json",
+      "Authorization": "token " + token
+    },
+  json={"ref": "master"}
 )
 
+print("\n == URL ==")
+print(r.request.url)
+
+print("\n == HEADERS ==")
+print(r.request.headers)
+
+
+print("\n == REQUEST BODY ==")
+print(r.request.body)
+
+
+print("\n == RESPONSE ==")
+try:
+  print(r.json())
+except:
+  print(r.raw.headers['Status'])
